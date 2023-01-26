1411e45ff6a6863dc7e71007fbd507305e686af2
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 19:05:44 +0100

    interface implemented

--- Library/classes/Offer/RefPriceOfferManager.cs
+++ Library/classes/Offer/RefPriceOfferManager.cs
@@ -1,6 +1,6 @@
 namespace Library.classes.Offer
 {
-    public class RefPriceOfferManager
+    public class RefPriceOfferManager : IOfferManager
     {
         private Dictionary<Int32, OfferSchema> offers;
         private List<OfferSchema> bestOffers;
@@ -9,6 +9,26 @@ namespace Library.classes.Offer
         public RefPriceOfferManager(){
             this.offers = new Dictionary<int, OfferSchema>();
             this.bestOffers = new List<OfferSchema>();
+        }
+
+        public OfferSchema addOffer(OfferSchema offer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, OfferSchema> getAllOffers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OfferSchema> getBestOffers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getListSize()
+        {
+            throw new NotImplementedException();
         }
     }
 }
