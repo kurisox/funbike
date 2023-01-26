97b20918540d8a031d00bebc0892238b2dc0d1f0
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:32:37 +0100

    singleton ctor added

--- Library/classes/Offer/Offer_Schema/OfferFactory.cs
+++ Library/classes/Offer/Offer_Schema/OfferFactory.cs
@@ -2,6 +2,10 @@ namespace Library.classes.Offer.Offer_Schema
 {
     public class OfferFactory
     {
-        
+        private static Random random;
+
+        private OfferFactory(){
+            random = new Random();
+        }
     }
 }
