8fe397ceafe784b439818053ea1592808e5af9d8
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 19:15:25 +0100

    properties and ctor added

--- Library/PriceComparison.cs
+++ Library/PriceComparison.cs
@@ -1,7 +1,26 @@
+using Library.classes.Calculation;
+using Library.classes.DataInput;
+using Library.classes.GUI;
+using Library.classes.Offer;
+
 namespace Library
 {
     public class PriceComparison : IPriceComparison
     {
+        private IOfferManager offerManager;
+        private IDataInputService dataInputService;
+        private ICalculationService calculationService;
+        private IGUIService gUIService;
+
+        private Boolean endProgram;
+
+        public PriceComparison(IOfferManager offerManager, IDataInputService dataInputService, ICalculationService calculationService, IGUIService gUIService){
+            this.offerManager = offerManager;
+            this.dataInputService = dataInputService;
+            this.calculationService = calculationService;
+            this.gUIService = gUIService;
+            this.endProgram = false;
+        }
         public void run()
         {
             throw new NotImplementedException();
