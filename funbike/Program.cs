f7ef006fd19c2ad2a70e1b6e290cf9bcbaf22f38
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 21:47:36 +0100

    dependency injection added

--- funbike/Program.cs
+++ funbike/Program.cs
@@ -3,14 +3,22 @@ using Library.classes.DataInput;
 using Library.classes.Offer;
 using Library.classes.Calculation;
 using Library.classes.GUI;
+using Autofac;
 
 namespace funbike
 {
     public class Program
     {
         static void Main(string[] args) {
-            IPriceComparison priceComparison = new PriceComparison(new RefPriceOfferManager(), new RefPriceInputService(new RefPriceGUIService()), new RefPriceCalcService(), new RefPriceGUIService());
-            priceComparison.run();
+            /*IPriceComparison priceComparison = new PriceComparison(new OfferManager(), new DataInputService(), new CalculationService(), new GUIService());
+            priceComparison.run();*/
+            
+            var container = ConfigContainer.Configure();
+
+            using (var scope = container.BeginLifetimeScope()){
+                var app = scope.Resolve<IApplication>();
+                app.run();
+            }
 
         }
     }
