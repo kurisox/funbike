cf87af4d410b094ee777c13719b7ae68fd61ceba
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:13:45 +0100

    properties added to the offer schema

--- Library/classes/Offer/Offer_Schema/Offer.cs
+++ Library/classes/Offer/Offer_Schema/Offer.cs
@@ -2,6 +2,15 @@ namespace Library.classes.Offer
 {
     public class Offer
     {
-        
+        public int offerID  {get; set; }
+        public double listPrice { get; set; }
+        public double deliveryDiscount { get; set; }
+        public double deliveryDiscountInEuro {get; set;}
+        public double targetPurchasePrice { get; set; }
+        public double deliverySkonto { get; set; }
+        public double deliverySkontoInEuro {get; set;}
+        public double cashPurchasePrice { get; set; }
+        public double referenceCosts { get; set; }
+        public double referencePrice { get; set; }
     }
 }
