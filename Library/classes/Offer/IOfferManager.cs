7bf5d70de8fbb0fa48e841ab0dbc9c406765281a
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 19:03:59 +0100

    methods added

--- Library/classes/Offer/IOfferManager.cs
+++ Library/classes/Offer/IOfferManager.cs
@@ -2,6 +2,9 @@ namespace Library.classes.Offer
 {
     public interface IOfferManager
     {
-         
+        OfferSchema addOffer(OfferSchema offer);
+        int getListSize();
+        List<OfferSchema> getBestOffers();
+        Dictionary<Int32, OfferSchema> getAllOffers();
     }
 }
