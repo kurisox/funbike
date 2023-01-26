5f92135bb6213b4dc10df02224a4bf75dd7164eb
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:34:30 +0100

    createOffer method added and implemented

--- Library/classes/Offer/Offer_Schema/OfferFactory.cs
+++ Library/classes/Offer/Offer_Schema/OfferFactory.cs
@@ -7,5 +7,9 @@ namespace Library.classes.Offer.Offer_Schema
         private OfferFactory(){
             random = new Random();
         }
+
+        public static OfferSchema createOffer(double listPrice, double deliveryDiscount, double deliverySkonto, double referenceCosts){
+            return new OfferSchema(random.Next(1,10000), listPrice, deliveryDiscount, deliverySkonto, referenceCosts);
+        }
     }
 }
