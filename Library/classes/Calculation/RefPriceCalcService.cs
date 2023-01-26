93ce01197c1629ca4f7ceaf626f1c344083b2730
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 20:19:10 +0100

    typo in addOffer removed

--- Library/classes/Calculation/RefPriceCalcService.cs
+++ Library/classes/Calculation/RefPriceCalcService.cs
@@ -8,7 +8,7 @@ namespace Library.classes.Calculation
         {
             offer.deliveryDiscountInEuro = calcDiscountsInEuro(offer.listPrice, offer.deliveryDiscount);
             offer.targetPurchasePrice = calcWithFixedCosts(offer.listPrice, (-1) * offer.deliveryDiscountInEuro);
-            offer.deliverySkontoInEuro = calcDiscountsInEuro(offer.targetPurchasePrice, offer.deliveryDiscount);
+            offer.deliverySkontoInEuro = calcDiscountsInEuro(offer.targetPurchasePrice, offer.deliverySkonto);
             offer.cashPurchasePrice = calcWithFixedCosts(offer.targetPurchasePrice, (-1) * offer.deliverySkontoInEuro);
             offer.referencePrice = calcWithFixedCosts(offer.cashPurchasePrice, offer.referenceCosts);
             return offer;
