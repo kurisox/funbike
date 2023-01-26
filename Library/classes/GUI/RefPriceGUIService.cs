eba368ea6415ef3f32c48c2d1b0d4152723f9383
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:57:30 +0100

    IGUIService implemented

--- Library/classes/GUI/RefPriceGUIService.cs
+++ Library/classes/GUI/RefPriceGUIService.cs
@@ -1,7 +1,52 @@
+using Library.classes.Offer;
+
 namespace Library.classes.GUI
 {
-    public class RefPriceGUIService
+    public class RefPriceGUIService : IGUIService
     {
-        
+        public int choiceMainMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void inputAdded()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void inputOutOfBounce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void offerAdded(bool added)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int offerMenu(int inputStep)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void printMainMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void showBestOffers(List<OfferSchema> offers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void showOffers(Dictionary<int, OfferSchema> offers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void wrongFormat()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
