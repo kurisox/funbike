26bec4ea0bbb1bd7e5f418c1245c1e26ea70a5c6
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:57:01 +0100

    service methods added

--- Library/classes/GUI/IGUIService.cs
+++ Library/classes/GUI/IGUIService.cs
@@ -1,7 +1,25 @@
+using Library.classes.Offer;
+
 namespace Library.classes.GUI
 {
     public interface IGUIService
     {
-         
+        void printMainMenu();
+
+        int choiceMainMenu();
+
+        int offerMenu(int inputStep);
+
+        void wrongFormat();
+
+        void inputOutOfBounce();
+
+        void inputAdded();
+
+        void offerAdded(Boolean added);
+
+        void showOffers(Dictionary<Int32, OfferSchema> offers);
+
+        void showBestOffers(List<OfferSchema> offers);
     }
 }
