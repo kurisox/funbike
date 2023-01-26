d1c53cdf1215c372bc24760a4a40c6ddab44f7f9
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:18:13 +0100

    interface ICalculation implemented

--- Library/classes/Calculation/RefPriceCalcService.cs
+++ Library/classes/Calculation/RefPriceCalcService.cs
@@ -1,7 +1,12 @@
+using Library.classes.Offer;
+
 namespace Library.classes.Calculation
 {
-    public class RefPriceCalc
+    public class RefPriceCalc : ICalculation
     {
-        
+        public OfferSchema calcOffer(OfferSchema offer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
