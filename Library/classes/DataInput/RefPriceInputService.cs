2a22229b5dcbcc429037f6f766a1108d9488bc11
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:48:07 +0100

    methoded added and implemented to add fixed values

--- Library/classes/DataInput/RefPriceInputService.cs
+++ Library/classes/DataInput/RefPriceInputService.cs
@@ -1,3 +1,4 @@
+using Library.classes.Exceptions;
 using Library.classes.Offer;
 
 namespace Library.classes.DataInput
@@ -7,6 +8,23 @@ namespace Library.classes.DataInput
         public OfferSchema dataInput()
         {
             throw new NotImplementedException();
+        }
+
+        private double addFixedValue(int step){
+            double fixedValue = 0;
+            Boolean inputFinshed = false;
+            do{
+                try{
+                    fixedValue = Convert.ToDouble(System.Console.ReadLine());
+                    if(fixedValue < 0){
+                        throw new InvalidNumberException("Eingabe kleiner als 0");
+                    }
+                }catch(InvalidNumberException ex){
+                }catch(FormatException ex){
+                }
+            }while(!inputFinshed);
+
+            return fixedValue;
         }
     }
 }
