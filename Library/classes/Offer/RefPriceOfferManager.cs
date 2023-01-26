e7be450f162924094fff5cae4a8336fa5f5915cb
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 19:05:09 +0100

    properties and ctor added

--- Library/classes/Offer/RefPriceOfferManager.cs
+++ Library/classes/Offer/RefPriceOfferManager.cs
@@ -2,6 +2,13 @@ namespace Library.classes.Offer
 {
     public class RefPriceOfferManager
     {
-        
+        private Dictionary<Int32, OfferSchema> offers;
+        private List<OfferSchema> bestOffers;
+        private int listSize = 5;
+
+        public RefPriceOfferManager(){
+            this.offers = new Dictionary<int, OfferSchema>();
+            this.bestOffers = new List<OfferSchema>();
+        }
     }
 }
