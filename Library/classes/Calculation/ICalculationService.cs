f920a4326f48ac2bac70759b224d811cb0642056
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:16:47 +0100

    calcOffer-method added

--- Library/classes/Calculation/ICalculationService.cs
+++ Library/classes/Calculation/ICalculationService.cs
@@ -1,7 +1,9 @@
+using Library.classes.Offer;
+
 namespace Library.classes.Calculation
 {
     public interface ICalculation
     {
-         
+         OfferSchema calcOffer(OfferSchema offer);
     }
 }
