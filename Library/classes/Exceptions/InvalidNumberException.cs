c4ba7d0e58738021b0b9212bad65a8fdf22aa364
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-01-26 16:43:57 +0100

    basic exception and message exception added

--- Library/classes/Exceptions/InvalidNumberException.cs
+++ Library/classes/Exceptions/InvalidNumberException.cs
@@ -1,7 +1,13 @@
+using System;
 namespace Library.classes.Exceptions
 {
     public class InvalidNumberException : Exception
     {
-        
+        public InvalidNumberException(){}
+
+        public InvalidNumberException(string message)
+        : base(message)
+        {
+        }
     }
 }
