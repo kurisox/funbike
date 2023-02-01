bc34fb160fba18810de931a04498958e7cf8d4a1
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-02-01 12:47:22 +0100

    ctor added

--- Library/SimplePriceComparison.cs
+++ Library/SimplePriceComparison.cs
@@ -1,7 +1,26 @@
+using Library.classes.Calculation;
+using Library.classes.DataInput;
+using Library.classes.GUI;
+using Library.classes.Offer;
+
 namespace Library
 {
     public class SimplePriceComparison : IPriceComparison
     {
+
+        private IOfferManager offerManager;
+        private IDataInputService dataInputService;
+        private ICalculationService calculationService;
+        private IGUIService gUIService;
+
+        public SimplePriceComparison(IOfferManager offerManager, IDataInputService dataInputService, ICalculationService calculationService, IGUIService gUIService){
+            this.offerManager = offerManager;
+            this.dataInputService = dataInputService;
+            this.calculationService = calculationService;
+            this.gUIService = gUIService;
+        }
+
+
         public void run()
         {
             throw new NotImplementedException();
