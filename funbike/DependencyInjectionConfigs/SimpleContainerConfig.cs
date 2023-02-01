3b380d03d8e11a65e4e26974bfc0693a1456974f
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-02-01 13:36:28 +0100

    Configure Method implemented

--- funbike/DependencyInjectionConfigs/SimpleContainerConfig.cs
+++ funbike/DependencyInjectionConfigs/SimpleContainerConfig.cs
@@ -1,7 +1,25 @@
+using Autofac;
+using Library;
+using Library.classes.Calculation;
+using Library.classes.DataInput;
+using Library.classes.GUI;
+using Library.classes.Offer;
+
 namespace funbike.DependencyInjectionConfigs
 {
     public class SimpleContainerConfig
     {
-        
+        public static IContainer Configure(){
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Application>().As<IApplication>();
+            builder.RegisterType<SimplePriceComparison>().As<IPriceComparison>();
+            builder.RegisterType<SimpleOfferManager>().As<IOfferManager>();
+            builder.RegisterType<DataInputService>().As<IDataInputService>();
+            builder.RegisterType<CalculationService>().As<ICalculationService>();
+            builder.RegisterType<SimpleGUIService>().As<IGUIService>();
+
+            return builder.Build();
+        }
     }
 }
