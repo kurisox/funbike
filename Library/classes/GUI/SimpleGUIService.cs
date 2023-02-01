b11d8254dc32aedd0fb77e56cd06bcc96bbb228f
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-02-01 12:45:29 +0100

    initial commit

---
+++ Library/classes/GUI/SimpleGUIService.cs
@@ -0,0 +1,57 @@
+using Library.classes.Offer;
+
+namespace Library.classes.GUI
+{
+    public class SimpleGUIService : IGUIService
+    {
+        public int choiceMainMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void inputAdded()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void inputOutOfBounce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void inputOutOfBounce(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void offerAdded(bool added)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int offerMenu(int inputStep)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void printMainMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void showBestOffers(List<OfferSchema> offers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void showOffers(Dictionary<int, OfferSchema> offers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void wrongFormat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
